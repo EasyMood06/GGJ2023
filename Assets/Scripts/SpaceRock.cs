092b0ceb04970704c105ad76b3dbf14d97eefaf8
EasyMood06 <xuqikun0306@gmail.com>
2023-02-04 10:46:54 -0500

    Rock update

--- Assets/Scripts/SpaceRock.cs
+++ Assets/Scripts/SpaceRock.cs
@@ -6,19 +6,51 @@ public class SpaceRock : MonoBehaviour
 {
     int massCoefficient = 50;
     int forceCoefficient = 15;
+    float distanceForInitialization = 50;
     Vector2 unitDirection;
     float force;
     int randRorce;
     float randSize;
     int randDirection;
     Rigidbody2D rb2D;
+    bool isInitialized;
+    PlayerController player;
+    Vector3 initialPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
         RandomGeneration();
-        RockInitialization();
+        isInitialized = false;
+        player = FindObjectOfType<PlayerController>();
+        initialPosition = transform.position;
+    }
+
+    void Update()
+    {
+        // first initialize
+        if(!isInitialized)
+        {
+            // when distance between player and rock is less than 100, initialize
+            float distToPlayer = Vector3.Distance(player.transform.position, transform.position);
+            if(distToPlayer <= distanceForInitialization)
+            {
+                RockInitialization();
+                isInitialized = true;
+            }
+            return;
+        }
+        // regeneration, when this rock is far beyond the initial position, and player is not around
+        float distToInitialPosition = Vector3.Distance(transform.position, initialPosition);
+        if(distToInitialPosition >= distanceForInitialization)  // need to reset position
+        {
+            float distToPlayer = Vector3.Distance(player.transform.position, transform.position);
+            if(distToPlayer >= distanceForInitialization)
+            {
+                transform.position = initialPosition;
+            }
+        }
     }
 
     void RandomGeneration()
@@ -28,7 +60,7 @@ public class SpaceRock : MonoBehaviour
         randDirection = Random.Range(0, 360);
     }
 
-    void RockInitialization()
+    public void RockInitialization()
     {
         // size Transform.scale = random_size
         transform.localScale = new Vector3(randSize, randSize, randSize);
