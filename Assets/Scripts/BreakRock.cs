11e0939c3d8b18019038cc2c5563b950d523c295
Zerlock1001 <798164639@qq.com>
2023-02-05 11:29:30 +0800

    Rock Breaker Bug Fixed

--- Assets/Scripts/BreakRock.cs
+++ Assets/Scripts/BreakRock.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class BreakRock : MonoBehaviour
 {
     public float breakingVelocity = 8f;
+    public bool enable = false;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Rock")
+        if (collision.gameObject.tag == "Rock" && enable)
         {
             if (gameObject.GetComponent<Rigidbody2D>().velocity.magnitude >= breakingVelocity)
             {
                 Debug.Log("Breaking!");
-                collision.gameObject.SetActive(false);
+                collision.gameObject.transform.position = new Vector3(500, 500, 0);
             }
         }
     }
