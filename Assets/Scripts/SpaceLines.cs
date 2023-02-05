08c418918660d66148735177e08554b3afa196f2
EasyMood06 <xuqikun0306@gmail.com>
2023-02-04 21:08:02 -0500

    final show update

--- Assets/Scripts/SpaceLines.cs
+++ Assets/Scripts/SpaceLines.cs
@@ -10,6 +10,12 @@ public class SpaceLines : MonoBehaviour
     void Start()
     {
         smallSpaceships = FindObjectsOfType<SmallSpaceship>();
+        for(int i=0; i<smallSpaceships.Length-1; i++)
+        {
+            Vector3 startPosition = smallSpaceships[i].transform.position;
+            Vector3 endPosition = smallSpaceships[i+1].transform.position;
+            DrawSpaceLine(startPosition, endPosition);
+        }
     }
 
     // Update is called once per frame
@@ -21,5 +27,9 @@ public class SpaceLines : MonoBehaviour
     void DrawSpaceLine(Vector3 startPosition, Vector3 endPosition)
     {
         GameObject newLine = Instantiate(spaceLine, new Vector3(0,0,0), Quaternion.identity);
+        newLine.transform.parent = transform;
+        LineRenderer lineRenderer = newLine.GetComponent<LineRenderer>();
+        lineRenderer.SetPosition(0, startPosition);
+        lineRenderer.SetPosition(1, endPosition);
     }
 }
